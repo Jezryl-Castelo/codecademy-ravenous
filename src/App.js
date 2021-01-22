6eafa4d4f4a36235770d0beb5fcd4eee805e0fdb
Jezryl <yamabags@gmail.com>
2021-01-22 09:12:52 -0700

    connect api to button

--- src/App.js
+++ src/App.js
@@ -2,31 +2,49 @@ import React from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import BusinessList from './components/BusinessList/BusinessList';
+import Yelp from './util/Yelp';
 
-const business = {
-  imageSrc: 
-      'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
 
-const businesses = [
-  business, business, business, business, business, business
-];
+// const business = {
+//   imageSrc: 
+//       'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+
+// const businesses = [
+//   business, business, business, business, business, business
+// ];
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: [],
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    //console.log(`You are searching for ${term}, ${location}, and ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(businesses => {
+    this.setState({ businesses: businesses });
+    });
+  }
+
   render() {
     return (
       <div className="App">
       <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses} />
+      <SearchBar searchYelp={this.searchYelp}/>
+      <BusinessList businesses={this.state.businesses} />
     </div>
     );
   }
